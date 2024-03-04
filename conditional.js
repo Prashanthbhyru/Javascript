99577ca2e1875ae4946799df97c0570e2db18736
Prashnathbhyru <prashanthbhyru.com>
2024-03-04 21:39:47 +0530

    js code

---
+++ conditional.js
@@ -0,0 +1,138 @@
+// let age = 18;
+
+// if( age >= 18) {
+//   console.log("you can drive");
+// }
+// else{
+//     console.log("you cannot drive");
+// }
+
+// let mode = dark;
+
+
+
+// let age = 1;
+
+// if (age > 18){
+//     console.log("drive");
+// }
+
+// if (age < 18){
+//     console.log("don't drive");
+// }
+
+
+
+
+
+// let mode = "blue";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+
+// if (mode === "light"){
+//     color = "white"
+// }
+// if (mode === "blue"){
+//     color = "bluee"
+// }
+
+// console.log(color)
+
+// let mode = "blue";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+// else{
+//     color = "white"
+
+//  }
+// console.log(color)
+
+// let age = 17;
+ 
+// if (age > 18){
+//  console.log("Eligible")   
+// }
+
+// else{
+//     console.log("not Eligible");
+// }
+
+// let number = 22;
+
+// if( number%2 ===0 ){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+
+
+// let mode = "blue";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else if(mode === "blue"){
+//     color = "blue";
+// }
+// else if(mode === "eye"){
+//     color = "light red";
+// }
+// else if (mode === "white"){
+// color = "white";
+// }
+// else{
+//     color = "green"
+// }
+// console.log(color)
+
+//  let age = 19;
+
+//  let result = age >= 18 ? "adult" : " not adult";
+
+//  console.log(result)
+
+//  let name = prompt("hello")
+//  console.log(name);
+
+
+
+
+// let num = prompt("enter a number:");
+
+// if(num % 5 === 0){
+//     console.log("multiple of 5")
+// }
+// else{
+//     console.log("not a multiple of 5")
+// }
+
+
+
+let score = prompt("Enter your score"); 
+let Grade;
+
+if (score >= 90 && score <= 100){
+    Grade = "A";
+}
+else if (score >= 70 && score <=89 ){
+    Grade = "B";
+}
+else if (score >= 60 && score <= 69){
+    Grade = "C";
+}
+else if (score >= 50 && score <= 59){
+    Grade = "D"
+}
+    else if (score >= 0 && score <= 49){
+        Grade = "E";
+    }
+console.log(Grade);
