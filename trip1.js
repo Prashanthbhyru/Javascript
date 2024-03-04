9a60ad55c7e7d38f5efc5beba11a30ba5909a35e
Prashnathbhyru <prashanthbhyru.com>
2024-03-04 21:48:52 +0530

    codejs

---
+++ trip1.js
@@ -0,0 +1,24 @@
+let a=5;
+let b=2;
+
+// console.log("a=",a,"& b=",b)
+// console.log(a++);
+// console.log(a);
+// // console.log(++a)
+// // console.log(--a)
+
+
+// console.log(a==b)
+
+// logical operator
+
+// let cond1 = a > b;
+// let cond2 = a===b;
+// console.log(cond1 && cond2)
+// it give the output of false 
+
+// let cond1 = a > b;
+// let cond2 = a===b;
+// console.log(cond1 || cond2)
+
+console.log(!(a<b));
